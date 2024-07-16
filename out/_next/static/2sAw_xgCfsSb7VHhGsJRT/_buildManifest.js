bf4ef8a2b506e516909c72ff54eaca16ed4ffd90
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2024-07-16 23:34:04 +1200

    Delete out directory

--- out/_next/static/2sAw_xgCfsSb7VHhGsJRT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/356-a2572b1772254600.js","static/chunks/pages/index-ad9c5a36505fe11b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
